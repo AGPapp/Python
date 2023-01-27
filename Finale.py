87ec3104eeb0deb81fbad6f9d606f25b97fc933d
Andrey <agpopov@appline.ru>
2023-01-27 11:45:00 +0300

    Fin 2

--- Finale.py
+++ Finale.py
@@ -6,13 +6,13 @@ driver = webdriver.Chrome()
 driver.get("http://google.com/ncr")
 
 # 2. Выполнить поиск слова “selenide”
-driver.find_element('xpath', './/input[contains(@class, "gLFyf")]').send_keys('selenide\n')
+driver.find_element('name', 'q').send_keys('selenide\n')
 
 # 3. Проверить, что первый результат – ссылка на сайт selenide.org.
 assert 'selenide.org' in driver.find_element('tag name', 'cite').text
 
 # 4. Перейти в раздел поиска изображений
-driver.find_element('link text',"Images").click()
+driver.find_element('link text', "Images").click()
 
 # 5. Проверить, что первое изображение неким образом связано с сайтом selenide.org.
 assert "selenide.org" in driver.find_element('xpath', '//*[@id="islrg"]/div[1]/div[1]/a[2]/div').text
@@ -23,3 +23,5 @@ driver.find_element('link text', "Все").click()
 # 7. Проверить, что первый результат такой же, как и на шаге 3.
 assert 'selenide.org' in driver.find_element('tag name', 'cite').text
 
+driver.close()
+driver.quit()
